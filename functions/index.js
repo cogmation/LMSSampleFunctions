7cdda6457bc5121d1c8c0c58fc288bcafc428c64
Andrew <andrew@cogmation.com>
2019-12-12 15:53:18 -0800

    only has OnCreate for users.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
+'use strict';
 const functions = require('firebase-functions');
+const request = require('request');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const lmsID = "-LrtYiM1kNw7CueH8dZR";// ID of Cogmation Testing LMS, your ID will be provided by Cogmation.
+
+// When a new user is created, ensure VRT system has user as well.
+exports.addUserToVRT = functions.auth.user().onCreate((user) => {
+    var uid = user.uid;
+    return new Promise(function (resolve, reject) {
+        request({
+            url: functionsHost + addVRTUserURL,
+            method: "POST",
+            json: true,
+            body: { email: user.email, lmsID: lmsID }
+        }, (error, response, body) => {
+            if (error) {
+                console.log(error);
+                reject(error);
+            }
+            if (response.statusCode != 200) {
+                reject({ error: 'Invalid status code <' + response.statusCode + '>' })
+            }
+            resolve(body);
+        });
+    });
+});
